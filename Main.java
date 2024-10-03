5f43b0db7b3a3780b2e58cc6bdec1abcae5c1f70
JorgeDavidTorricoCopali <202200286@est.umss.edu>
2024-10-03 06:55:27 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Documentacion;
+/**
+@autor: Jorge David Torrico Copali
+@codigo sis: 202200286
+ * Clase principal que inicia la ejecución del programa.
+ */
+public class Main {
+
+    /**
+     * Método principal que crea una nueva instancia de InterfazGrafica.
+     * 
+     * @param args Argumentos pasados al ejecutar el programa.
+     */
+    public static void main(String[] args) {
+        new InterfazGrafica();
+    }
+}
